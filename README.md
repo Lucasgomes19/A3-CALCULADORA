d9ec49c652a71f3c520870f4c47bcf4bf5ce7485
Lucasgomes19 <lucasmadrid01@gmail.com>
2022-12-13 22:25:05 -0300

    A3-CALCULADORA

---
+++ README.md
@@ -0,0 +1,2 @@
+Lucas Emanuel da Silva Felix Gomes RA: 822157538
+Rafael Luna Machiavelli RA: 819157890
---
+++ src/Desenvolvedor.java
@@ -0,0 +1,19 @@
+public class Desenvolvedor extends Principal{
+    String nome = "Ednaldo Pereira";
+    double QHD = 8;
+     double VMD = 3000;
+   
+
+    private int diasUteis = 20;
+    private double fhd = 7;
+
+    public double horasUteisMes = diasUteis * fhd;
+
+    public double CalculaValorHora() {
+        double VH = 0;
+        int HDD = 8;
+        return VH = VMD/(20 * HDD) ;
+    }
+
+ 
+    }
